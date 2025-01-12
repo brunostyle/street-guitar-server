5d4d20c55374a86d6751a59a8ec1beb1c449a92c
brunostyle <brunomstyle@gmail.com>
2025-01-12 18:51:01 -0300

    updated

--- src/services/dashboard/controller.ts
+++ src/services/dashboard/controller.ts
@@ -6,7 +6,7 @@ import { Order } from "../orders/model";
 export class DashboardController {
     public getStats = async (req: Request, res: Response) => {
         try {
-            const [numbersOfClients, numbersOfProducts, numbersOfOrders, lastSells] = await Promise.all([
+            const [numberOfClients, numberOfProducts, numberOfOrders, lastSells] = await Promise.all([
                 User.countDocuments(),
                 Product.countDocuments(),
                 Order.find().countDocuments(),
@@ -19,7 +19,7 @@ export class DashboardController {
                 clients: [31, 40, 28, 51, 42, 109, 100],
                 sells: [11, 32, 45, 32, 34, 52, 41]
             }
-            res.json({ numbersOfClients, numbersOfProducts, numbersOfOrders, lastSells, chart });
+            res.json({ numberOfClients, numberOfProducts, numberOfOrders, lastSells, chart });
         } catch (error) {
             console.log(error);
         }
