5f44348f101c433730a3071fb1558f65cdd01893
brunostyle <brunomstyle@gmail.com>
2025-03-23 17:35:38 -0300

    chart data updated

--- src/services/dashboard/controller.ts
+++ src/services/dashboard/controller.ts
@@ -3,23 +3,37 @@ import { User } from "../users/model";
 import { Product } from "../products/model";
 import { Order } from "../orders/model";
 
+function groupBy<T, K>(array: T[], keySelector: (item: T) => K): Map<K, T[]> {
+    return array.reduce((map, item) => {
+        const key = keySelector(item);
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key)!.push(item);
+        return map;
+    }, new Map<K, T[]>());
+}
+
 export class DashboardController {
     public getStats = async (req: Request, res: Response) => {
         try {
-            const [numberOfClients, numberOfProducts, numberOfOrders, lastSells] = await Promise.all([
+            const [numberOfClients, numberOfProducts, numberOfOrders, lastSells, sells] = await Promise.all([
                 User.countDocuments(),
                 Product.countDocuments(),
                 Order.find().countDocuments(),
                 Order.find()
                     .limit(10)
                     .select('id createdAt')
-                    .populate('user', 'id name email avatar')
+                    .populate('user', 'id name email avatar'),
+                Order.find()
+                    .select('createdAt items')
             ])
-            const chart = {
-                clients: [31, 40, 28, 51, 42, 109, 100],
-                sells: [11, 32, 45, 32, 34, 52, 41]
+            const group = groupBy(sells, item => item.createdAt.toLocaleDateString('es-ES'));
+            const chartData = [];
+            for (const [time, values] of group.entries()) {
+                chartData.push({ time, value: values.reduce((acc, act) => acc + act.items, 0) })
             }
-            res.json({ numberOfClients, numberOfProducts, numberOfOrders, lastSells, chart });
+            res.json({ numberOfClients, numberOfProducts, numberOfOrders, lastSells, chartData });
         } catch (error) {
             console.log(error);
         }
